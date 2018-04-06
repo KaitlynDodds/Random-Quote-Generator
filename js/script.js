78bf5f7676eab18fc6da0d0f19e51190e2b80a0f
KaitlynDodds <kadodds.dev@kadodds.com>
2018-04-06 12:22:59 -0600

    new quote generated every 30 sec

--- js/script.js
+++ js/script.js
@@ -123,6 +123,14 @@ const printQuote = () => {
 }
 
 
+// load quote immediately 
+window.onload = printQuote();
+
+
+// reload quote every 30 sec
+setInterval(printQuote, 30000);
+
+
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
